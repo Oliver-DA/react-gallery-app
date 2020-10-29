a4e11dac04ea342915ba240be953a685181f1369
Oliver-DA <71343538+Oliver-DA@users.noreply.github.com>
2020-10-29 14:50:45 -0400

    First commit

---
+++ src/components/PhotoContainer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const PhotoContainer = () => {
+    return (
+
+        <div className = "photo-container">
+        <h2>Results</h2>
+            <ul>
+                {/*Here goes the photo*/}
+            </ul>
+        </div>
+    )
+}
+
+export default PhotoContainer;
